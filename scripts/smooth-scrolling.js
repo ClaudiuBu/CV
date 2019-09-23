40e292ffa88762d7433cb73651ac99bd1403182d
ClaudiuBu <37150852+ClaudiuBu@users.noreply.github.com>
2019-09-23 11:07:12 +0300

    Add files via upload
    Implemented Smooth Scrolling via jQuery Script.

---
+++ scripts/smooth-scrolling.js
@@ -0,0 +1,19 @@
+
+$( '.page-bar a' ).on( 'click', function(e){
+    var href = $(this).attr( 'href' );
+    $( 'html, body' ).animate({
+          scrollTop: $( href ).offset().top
+    }, '600' );
+    e.preventDefault();
+  
+  });
+
+$( '.icon-header-container a' ).on( 'click', function(e){
+	
+    var href = $(this).attr( 'href' );
+    $( 'html, body' ).animate({
+          scrollTop: $( href ).offset().top
+    }, '600' );
+    e.preventDefault();
+  
+  });
