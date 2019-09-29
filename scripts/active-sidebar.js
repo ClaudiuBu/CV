9c857960700a29fd371c3b4b3e8efc20a8d12353
ClaudiuBu <37150852+ClaudiuBu@users.noreply.github.com>
2019-09-29 19:34:53 +0300

    Side-bar li active class will change as well depending on content in view. Fixed bug that occured on mobile devices.Was Unable to scroll all the way down. It was caused by active bar script. Script will be start  when device width is larger than 1200px, otherwise it will stay put.

--- scripts/active-sidebar.js
+++ scripts/active-sidebar.js
@@ -1,8 +1,13 @@
 
 
 $(document).ready(function () {
-    $(document).on("scroll", onScroll);
-
+    var deviceWidth= parseInt(window.innerWidth);
+    console.log(deviceWidth);
+    if(deviceWidth >=1200){
+        $(document).on("scroll", onScroll);
+    }
+    else return;
+   
     
 });
 
@@ -14,11 +19,21 @@ function onScroll(event){
         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
             $('#side-bar ul li a').removeClass("active");
             currLink.addClass("active");
-            //  $('#side-bar li')[n].addClass("active");
         }
         else{
             currLink.removeClass("active");
-            //  $('#side-bar li')[n].removeClass("active");
         }
     });
+    $('#side-bar li').each(function(){
+        var currLI =$(this);
+        var refElLI = $(currLI.attr("href"));
+        if (refElLI.position().top <= scrollPos && refElLI.position().top + refElLI.height() > scrollPos) {
+            $('#side-bar ul li').removeClass("li-active");
+            currLI.addClass("li-active");
+            
+        }
+        else{
+            currLI.removeClass("li-active");
+        }
+    })
 }
